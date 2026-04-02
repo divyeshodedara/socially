9517a859dc467e7eddb464af87f55cb7d761222c
divyeshodedara <divyeshvisana123@gmail.com>
2026-04-02 23:37:27 +0530

    fist commit

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const startServer = async () => {
     await mongoose.connect(process.env.DB_URL);
     console.log("MongoDB connected successfully!");
 
-    const server = app.listen(PORT, () => {
+    const server = app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
     });
 
